0d8ad046a15ff55608fa07358d5ef607b3c5c615
Martyna Gudwilowicz <m.gudwilowicz@gmail.com>
2025-11-17 22:10:48 +0100

    Add play again logic

--- snowman.py
+++ snowman.py
@@ -2,4 +2,9 @@ from game_logic import play_game
 
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+        again = input("Do you want to play again? (y/n):").lower()
+        if again != "y":
+            print("Goodbye!")
+            break
